ff40747516cd0a49e69fe4ba18c92beb952f6f4d
Lhourquin <lhourquin@gmail.com>
2019-08-19 04:09:22 +0200

    insert bone of html for each page, create new file html and js, insert import and export on appropriate file

---
+++ js/first-test.js
@@ -0,0 +1,7 @@
+/* Import / Export */
+
+// IMPORT PART OF A MODULE
+
+import { square, diag } from './lib.js';
+console.log(square(11)); 
+console.log(diag(4, 3));
---
+++ js/second-test.js
@@ -0,0 +1,7 @@
+/* Import / Export */
+
+// IMPORTING COMPLETE MODULE
+
+import * as lib from './lib.js';
+console.log(lib.square(11));
+console.log(lib.diag(4, 3)); 
